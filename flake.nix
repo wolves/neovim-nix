b3961248f49174cce80c7bf081a4d4c8da36350f
wolves <cstingl@protonmail.com>
2025-02-09 19:36:48 -0500

    Add neovim-nix-utls for creating neovim editions

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
     utils,
     haumea,
     neovimNightlyOverlay,
+    neovim-nix-utils,
     ...
   }:
     utils.lib.eachDefaultSystem (
@@ -16,10 +17,12 @@
           config = {allowUnfree = true;};
           overlays = [neovimNightlyOverlay.overlays.default];
         };
+        neovimNixLib = neovim-nix-utils.lib.${system};
       in (haumea.lib.load {
         src = ./src;
         inputs = {
           inherit pkgs;
+          inherit neovimNixLib;
         };
         transformer = haumea.lib.transformers.liftDefault;
       })
@@ -34,6 +37,10 @@
     };
     neovimNightlyOverlay = {
       url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    neovim-nix-utils = {
+      url = "github:PrimaMateria/neovim-nix-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
