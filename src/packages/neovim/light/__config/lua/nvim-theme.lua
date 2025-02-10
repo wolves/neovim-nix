8b2a2ab2f7b7bccedbc1c2a5f622fa7ad2c9a378
wolves <cstingl@protonmail.com>
2025-02-10 07:35:20 -0500

    Start fleshing out light edition

---
+++ src/packages/neovim/light/__config/lua/nvim-theme.lua
@@ -0,0 +1,20 @@
+require("kanagawa").setup({
+  compile = false,
+  undercurl = true, -- enable undercurls
+  commentStyle = { italic = true },
+  functionStyle = {},
+  keywordStyle = { italic = true },
+  statementStyle = { bold = true },
+  typeStyle = {},
+  transparent = false, -- do not set background color
+  dimInactive = true,
+  globalStatus = true,
+  colors = {},
+  theme = "wave",
+  background = {
+    dark = "wave",
+    light = "lotus",
+  },
+
+  vim.cmd("colorscheme kanagawa-wave")
+})
---
+++ src/packages/neovim/light/_plugins.nix
@@ -0,0 +1,18 @@
+{pkgs}:
+with pkgs.vimPlugins; [
+  # Look & Feel
+  # ----------------
+  kanagawa-nvim
+  lualine-nvim
+  noice-nvim
+  nvim-notify
+  which-key-nvim
+
+  # Navigation
+  # ----------------
+  harpoon
+  mini-nvim
+  telescope-frecency-nvim
+  telescope-fzf-native-nvim
+  telescope-nvim
+]
