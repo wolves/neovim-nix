5ecd54c90b78a2f589d3790b3b2fa5597bad893d
wolves <cstingl@protonmail.com>
2025-02-09 19:28:02 -0500

    Init commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "WLVS NeoVim Editions Flake";
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    haumea,
+    ...
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {allowUnfree = true;};
+        };
+      in (haumea.lib.load {
+        src = ./src;
+        inputs = {
+          inherit pkgs;
+        };
+        transformer = haumea.lib.transformers.liftDefault;
+      })
+    );
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/master";
+    utils.url = "github:numtide/flake-utils";
+    haumea = {
+      url = "github:nix-community/haumea";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+}
