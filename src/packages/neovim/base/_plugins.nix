1c7c427de6810a44eac472308cb636f1203edd2f
Wolves <cstingl@protonmail.com>
2025-02-10 15:34:23 -0500

    Base: Begin scaffolding base edition

---
+++ src/packages/neovim/base/_plugins.nix
@@ -0,0 +1,22 @@
+{pkgs}:
+with pkgs.vimPlugins; [
+  # Look & Feel
+  # ----------------
+
+  # Completion
+  # ----------------
+  blink-cmp
+
+  # Linting & Formatting
+  # ----------------
+
+  # LSP
+  # ----------------
+
+  # Text Editing
+  # ----------------
+  treesj
+  
+  # Version Control
+  # ----------------
+]
